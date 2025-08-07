7d3160ac7a72df572885befc0e72b031df952b1a
Canmi <9997200@qq.com>
2025-08-07 16:02:28 +0800

    add: tailwind v3

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="text-red-500 text-3xl font-bold">Tailwind Works!</div>
 </template>
 
 <style scoped></style>
